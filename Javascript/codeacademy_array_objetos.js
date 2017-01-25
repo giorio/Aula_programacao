613dbda52a7fd743f924b99186aea6a8bc53bf3e
GUSTAVO PADRAO DI IORIO AGUIAR <gpiorio@gmail.com>
2017-01-25 16:58:36 -0200

    Create codeacademy_array_objetos.js

---
+++ Javascript/codeacademy_array_objetos.js
@@ -0,0 +1,42 @@
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+
+for (var i = 0; i <= languages.length; i++) {
+	console.log(languages[i]);
+}
+
+var myArray = [29 ,true, "Gustavo"];
+
+var newArray = [["Gustavo", 29, true], ["Cynthia", 27, true], ["Lucas", 23, false]];
+
+var jagged = [["Gustavo", 29, true], ["CodeAcademy"],["PHP", 14, 16, true],["Programando", false]];
+
+var me = new Object();
+	me.name = "Gustavo";
+	me["age"] = 29;
+
+
+var object1 = {
+    teste: "Gustavo",
+};
+var object2 = new Object();
+    object2.name = "Cynthia";
+var object3 = new Object();
+    object3.age = 27;
+
+var myArray = [29, true, "Gustavo", [me];
+var me = new Object();
+	me.name = "Gustavo";
+	me["age"] = 29;
+
+var myObject = {
+  name: 'Eduardo',
+  type: 'Excelente',
+  // Adicione seu codigo aqui!
+  interests: [29, true, 'PHP']
+};
+
+var myOwnObject = new Object();
+	myOwnObject.name = "Gustavo";
+	myOwnObject.sister = "Cynthia";
+	myOwnObject.age = 29;
+	myOwnObject.sisterage = 27;
