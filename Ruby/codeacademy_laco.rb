0b0fe13b6ee179d29e30428c1bb6687cdc699e5e
GUSTAVO PADRAO DI IORIO AGUIAR <gpiorio@gmail.com>
2017-02-20 16:58:31 -0300

    Laços em Ruby
    Inicio da lição de Laços em Ruby.

---
+++ Ruby/codeacademy_laco.rb
@@ -0,0 +1,17 @@
+#laço while
+
+i = 0
+while i < 5
+    puts i
+    i = i +1
+end
+
+#laço until
+
+counter = 1
+until counter < 10
+  puts counter
+  # Adicione o código para atualizar o contador 'counter' aqui!
+  counter += 1
+  
+end
